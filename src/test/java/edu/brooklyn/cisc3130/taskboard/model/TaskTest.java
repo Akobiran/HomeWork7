241a81bd605189a8a53360acb9e0df6b8e57aafc
akobirrakhimov <@Akobiran>
2026-04-15 21:18:34 -0400

    First commit

---
+++ src/test/java/edu/brooklyn/cisc3130/taskboard/model/TaskTest.java
@@ -0,0 +1,12 @@
+package edu.brooklyn.cisc3130.taskboard.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskTest {
+
+    @Test
+    void getId() {
+    }
+}
